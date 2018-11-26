6612c1ce7b6c57956aa3e3b75e67cae5333e76ec
Vishal Nayak <35347836+vishalnayak108@users.noreply.github.com>
2018-11-27 03:04:33 +0530

    Update link.py
    Remove whitespace, newline character, tab etc.

--- links/spiders/link.py
+++ links/spiders/link.py
@@ -8,6 +8,13 @@ class LinkSpider(CrawlSpider):
     custom_settings = {
         'LOG_FILE': 'output.log',
     }
+    def __normalise(self, value):
+		# Convert list to string
+	    value = value if type(value) is not list else ' '.join(value)
+		# Trim leading and trailing special characters (Whitespaces, newlines, spaces, tabs, carriage returns)
+	    value = " ".join(value.split())
+	    return value 
+    
     def __init__(self, domain='', *args, **kwargs):
         super(LinkSpider, self).__init__(*args, **kwargs)
         self.start_urls = [domain]
@@ -19,4 +26,7 @@ class LinkSpider(CrawlSpider):
         item = LinksItem()
         item['page'] = response.url
         item['content'] = response.xpath('//p/text()').extract()
+        #remove white space, newline character, tab character etc.
+        item['content']= self.__normalise(item['content'])
+        
         yield item
