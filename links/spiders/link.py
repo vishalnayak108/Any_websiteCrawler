b84f5de4a20db3e64fec17e8ec6e32de1c40eab0
Vishal Nayak <35347836+vishalnayak108@users.noreply.github.com>
2018-11-27 01:11:48 +0530

    Update link.py

--- links/spiders/link.py
+++ links/spiders/link.py
@@ -5,6 +5,9 @@ from links.items import LinksItem
 
 class LinkSpider(CrawlSpider):
     name = "link"
+    custom_settings = {
+        'LOG_FILE': 'output.log',
+    }
     def __init__(self, domain='', *args, **kwargs):
         super(LinkSpider, self).__init__(*args, **kwargs)
         self.start_urls = [domain]
@@ -12,6 +15,7 @@ class LinkSpider(CrawlSpider):
     rules = (Rule(LinkExtractor(), callback='parse_page', follow=True),)
 
     def parse_page(self, response):
+        self.logger.info('Parse function called on %s', response.url)
         item = LinksItem()
         item['page'] = response.url
         item['content'] = response.xpath('//p/text()').extract()
